b51aeab66c4f1a7321f5ddc77d260a75dca4b49f
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-11-08 09:39:06 +0530

    [MH3-86274] berkeley_udp_relay application regeneration - PIC32CZ_CA_freertos

--- apps/berkeley_udp_relay/firmware/src/config/pic32cz_ca80_curiosity_ultra_freertos/driver/gmac/src/dynamic/drv_gmac_lib_pic32cz.h
+++ apps/berkeley_udp_relay/firmware/src/config/pic32cz_ca80_curiosity_ultra_freertos/driver/gmac/src/dynamic/drv_gmac_lib_pic32cz.h
@@ -16,30 +16,28 @@
     on Microchip PIC32CZ family 
 *******************************************************************************/
 //DOM-IGNORE-BEGIN
-/*****************************************************************************
- Copyright (C) 2022 Microchip Technology Inc. and its subsidiaries.
+/*
+Copyright (C) 2022-2023, Microchip Technology Inc., and its subsidiaries. All rights reserved.
 
-Microchip Technology Inc. and its subsidiaries.
+The software and documentation is provided by microchip and its contributors
+"as is" and any express, implied or statutory warranties, including, but not
+limited to, the implied warranties of merchantability, fitness for a particular
+purpose and non-infringement of third party intellectual property rights are
+disclaimed to the fullest extent permitted by law. In no event shall microchip
+or its contributors be liable for any direct, indirect, incidental, special,
+exemplary, or consequential damages (including, but not limited to, procurement
+of substitute goods or services; loss of use, data, or profits; or business
+interruption) however caused and on any theory of liability, whether in contract,
+strict liability, or tort (including negligence or otherwise) arising in any way
+out of the use of the software and documentation, even if advised of the
+possibility of such damage.
 
-Subject to your compliance with these terms, you may use Microchip software 
-and any derivatives exclusively with Microchip products. It is your 
-responsibility to comply with third party license terms applicable to your 
-use of third party software (including open source software) that may 
-accompany Microchip software.
-
-THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
-WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR 
-PURPOSE.
-
-IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
-BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE 
-FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN 
-ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, 
-THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*****************************************************************************/
+Except as expressly permitted hereunder and subject to the applicable license terms
+for any third-party software incorporated in the software and any applicable open
+source software license terms, no license or other rights, whether express or
+implied, are granted under any patent or other intellectual property rights of
+Microchip or any third party.
+*/
 
 //DOM-IGNORE-END
 
@@ -62,71 +60,71 @@ THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 */
 typedef enum{
     // All events
-	GMAC_EV_NONE
+    GMAC_EV_NONE
     /*DOM-IGNORE-BEGIN*/ = 0 /*DOM-IGNORE-END*/,
-	// Management Frame Sent
-	GMAC_EV_MGMTFRAMESENT
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_MFS_Msk /*DOM-IGNORE-END*/,
-	
-	// Receive Complete
-	GMAC_EV_RXCOMPLETE
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_RCOMP_Msk /*DOM-IGNORE-END*/,
-	
-	// RX Used Bit Read
-	GMAC_EV_RXUSEDBITREAD
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_RXUBR_Msk /*DOM-IGNORE-END*/,
-	
-	// TX Used Bit Read
-	GMAC_EV_TXUSEDBITREAD
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_TXUBR_Msk /*DOM-IGNORE-END*/,
-	
-	// Transmit Underrun
-	GMAC_EV_TXUNDERRUN
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_TUR_Msk /*DOM-IGNORE-END*/,
-	
-	// Retry Limit Exceeded
-	GMAC_EV_RETRYLIMITEXCEED
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_RLEX_Msk /*DOM-IGNORE-END*/,
-	
-	// Transmit Frame Corruption
-	GMAC_EV_TXFRAMECORRUPT
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_TFC_Msk /*DOM-IGNORE-END*/,
-	
-	// Transmit Complete
-	GMAC_EV_TXCOMPLETE
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_TCOMP_Msk /*DOM-IGNORE-END*/,
+    // Management Frame Sent
+    GMAC_EV_MGMTFRAMESENT
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_MFS_Msk /*DOM-IGNORE-END*/,
+    
+    // Receive Complete
+    GMAC_EV_RXCOMPLETE
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_RCOMP_Msk /*DOM-IGNORE-END*/,
+    
+    // RX Used Bit Read
+    GMAC_EV_RXUSEDBITREAD
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_RXUBR_Msk /*DOM-IGNORE-END*/,
+    
+    // TX Used Bit Read
+    GMAC_EV_TXUSEDBITREAD
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_TXUBR_Msk /*DOM-IGNORE-END*/,
+    
+    // Transmit Underrun
+    GMAC_EV_TXUNDERRUN
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_TUR_Msk /*DOM-IGNORE-END*/,
+    
+    // Retry Limit Exceeded
+    GMAC_EV_RETRYLIMITEXCEED
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_RLEX_Msk /*DOM-IGNORE-END*/,
+    
+    // Transmit Frame Corruption
+    GMAC_EV_TXFRAMECORRUPT
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_TFC_Msk /*DOM-IGNORE-END*/,
+    
+    // Transmit Complete
+    GMAC_EV_TXCOMPLETE
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_TCOMP_Msk /*DOM-IGNORE-END*/,
 
-	// Receive Overrun
-	GMAC_EV_RXOVERRUN
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_ROVR_Msk /*DOM-IGNORE-END*/,
+    // Receive Overrun
+    GMAC_EV_RXOVERRUN
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_ROVR_Msk /*DOM-IGNORE-END*/,
 
-	// HRESP Not OK
-	GMAC_EV_HRESPNOTOK
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_HRESP_Msk /*DOM-IGNORE-END*/,
+    // HRESP Not OK
+    GMAC_EV_HRESPNOTOK
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_HRESP_Msk /*DOM-IGNORE-END*/,
 
-	// Pause Frame with Non-zero Pause Quantum Received
-	GMAC_EV_PAUSEFRAMENZRECV
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_PFNZ_Msk /*DOM-IGNORE-END*/,
+    // Pause Frame with Non-zero Pause Quantum Received
+    GMAC_EV_PAUSEFRAMENZRECV
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_PFNZ_Msk /*DOM-IGNORE-END*/,
 
-	// Pause Time Zero
-	GMAC_EV_PAUSETIMEZERO
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_PTZ_Msk /*DOM-IGNORE-END*/,
+    // Pause Time Zero
+    GMAC_EV_PAUSETIMEZERO
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_PTZ_Msk /*DOM-IGNORE-END*/,
 
-	// Pause Frame Transmitted
-	GMAC_EV_PAUSEFRAMETX
-	/*DOM-IGNORE-BEGIN*/ = ETH_ISR_PFTR_Msk /*DOM-IGNORE-END*/,
-	// All events
-	GMAC_EV_ALL
-	/*DOM-IGNORE-BEGIN*/= (	GMAC_EV_MGMTFRAMESENT | GMAC_EV_RXCOMPLETE |  GMAC_EV_RXUSEDBITREAD |
-	GMAC_EV_TXUNDERRUN | GMAC_EV_RETRYLIMITEXCEED |	GMAC_EV_TXFRAMECORRUPT |
-	GMAC_EV_TXCOMPLETE | GMAC_EV_RXOVERRUN | GMAC_EV_HRESPNOTOK |
-	GMAC_EV_PAUSEFRAMENZRECV | GMAC_EV_PAUSETIMEZERO | GMAC_EV_PAUSEFRAMETX ) /*DOM-IGNORE-END*/,
+    // Pause Frame Transmitted
+    GMAC_EV_PAUSEFRAMETX
+    /*DOM-IGNORE-BEGIN*/ = ETH_ISR_PFTR_Msk /*DOM-IGNORE-END*/,
+    // All events
+    GMAC_EV_ALL
+    /*DOM-IGNORE-BEGIN*/= ( GMAC_EV_MGMTFRAMESENT | GMAC_EV_RXCOMPLETE |  GMAC_EV_RXUSEDBITREAD |
+    GMAC_EV_TXUNDERRUN | GMAC_EV_RETRYLIMITEXCEED | GMAC_EV_TXFRAMECORRUPT |
+    GMAC_EV_TXCOMPLETE | GMAC_EV_RXOVERRUN | GMAC_EV_HRESPNOTOK |
+    GMAC_EV_PAUSEFRAMENZRECV | GMAC_EV_PAUSETIMEZERO | GMAC_EV_PAUSEFRAMETX ) /*DOM-IGNORE-END*/,
     // RX events
-	GMAC_EV_RX_ALL
-	/*DOM-IGNORE-BEGIN*/= (GMAC_EV_RXCOMPLETE | GMAC_EV_RXUSEDBITREAD | GMAC_EV_RXOVERRUN) /*DOM-IGNORE-END*/, 
+    GMAC_EV_RX_ALL
+    /*DOM-IGNORE-BEGIN*/= (GMAC_EV_RXCOMPLETE | GMAC_EV_RXUSEDBITREAD | GMAC_EV_RXOVERRUN) /*DOM-IGNORE-END*/, 
     // TX events
-	GMAC_EV_TX_ALL
-	/*DOM-IGNORE-BEGIN*/= (GMAC_EV_TXCOMPLETE | GMAC_EV_TXFRAMECORRUPT | GMAC_EV_TXUNDERRUN) /*DOM-IGNORE-END*/ 
+    GMAC_EV_TX_ALL
+    /*DOM-IGNORE-BEGIN*/= (GMAC_EV_TXCOMPLETE | GMAC_EV_TXFRAMECORRUPT | GMAC_EV_TXUNDERRUN) /*DOM-IGNORE-END*/ 
 } GMAC_EVENTS;
 
 // *****************************************************************************
@@ -153,8 +151,12 @@ typedef struct
 /// The buffer addresses written into the descriptors must be aligned so the
 /// last few bits are zero.  These bits have special meaning for the GMAC
 /// peripheral and cannot be used as part of the address.
-#define GMAC_RX_ADDRESS_MASK   ((unsigned int)0xFFFFFFFC)
-#define GMAC_LENGTH_FRAME   ((unsigned int)0x3FFF)    /// Length of frame mask
+#define GMAC_RX_ADDRESS_MASK    ((unsigned int)0xFFFFFFFC)
+#define GMAC_LENGTH_FRAME       ((unsigned int)0x3FFF)    /// Length of frame mask
+#define GMAC_RX_CHECKSUM_OFFLOAD_STATUS     ((unsigned int)0x00C00000)
+#define GMAC_RX_CHECKSUM_IP_OFFLOAD         ((unsigned int)0x00400000)
+#define GMAC_RX_CHECKSUM_IP_TCP_OFFLOAD     ((unsigned int)0x00800000)
+#define GMAC_RX_CHECKSUM_IP_UDP_OFFLOAD     ((unsigned int)0x00C00000)
 
 // receive buffer descriptor bits
 #define GMAC_RX_OWNERSHIP_BIT   (1u <<  0)
@@ -219,14 +221,14 @@ typedef enum{
 
     // Jumbo frames accepted
     GMAC_FILT_JUMBOFRAME_ACCEPT
-    /*DOM-IGNORE-BEGIN*/ = ETH_NCFGR_JFRAME_Msk /*DOM-IGNORE-END*/,	
+    /*DOM-IGNORE-BEGIN*/ = ETH_NCFGR_JFRAME_Msk /*DOM-IGNORE-END*/, 
 
     GMAC_FILT_ALL_FILTERS
-    /*DOM-IGNORE-BEGIN*/    =	GMAC_FILT_PREAMBLE_ERR_ACCEPT	|	GMAC_FILT_CRC_ERR_ACCEPT	|
-                                GMAC_FILT_MAXFRAME_ACCEPT		|	GMAC_FILT_UCASTHASH_ACCEPT	|
-                                GMAC_FILT_MCASTHASH_ACCEPT		|	GMAC_FILT_BCAST_REJECT		|
-                                GMAC_FILT_ALLFRAME_ACCEPT		|	GMAC_FILT_ALLFRAME_ACCEPT	|
-                                GMAC_FILT_JUMBOFRAME_ACCEPT	/*DOM-IGNORE-END*/
+    /*DOM-IGNORE-BEGIN*/    =   GMAC_FILT_PREAMBLE_ERR_ACCEPT   |   GMAC_FILT_CRC_ERR_ACCEPT    |
+                                GMAC_FILT_MAXFRAME_ACCEPT       |   GMAC_FILT_UCASTHASH_ACCEPT  |
+                                GMAC_FILT_MCASTHASH_ACCEPT      |   GMAC_FILT_BCAST_REJECT      |
+                                GMAC_FILT_ALLFRAME_ACCEPT       |   GMAC_FILT_ALLFRAME_ACCEPT   |
+                                GMAC_FILT_JUMBOFRAME_ACCEPT /*DOM-IGNORE-END*/
 } GMAC_RX_FILTERS;
 
 
