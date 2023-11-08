076bf22dbeee43c1beca8cde16dc7e5f150b4805
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-11-08 09:21:29 +0530

    [MH3-86266] berkeley_tcp_server application regeneration - PIC32CZ_CA_freertos

--- apps/berkeley_tcp_server/firmware/src/config/pic32cz_ca80_curiosity_ultra/driver/gmac/src/dynamic/drv_gmac_lib.h
+++ apps/berkeley_tcp_server/firmware/src/config/pic32cz_ca80_curiosity_ultra/driver/gmac/src/dynamic/drv_gmac_lib.h
@@ -19,30 +19,28 @@
     thus hiding differences from one microcontroller variant to another.
 *******************************************************************************/
 //DOM-IGNORE-BEGIN
-/*****************************************************************************
- Copyright (C) 2017-2018 Microchip Technology Inc. and its subsidiaries.
+/*
+Copyright (C) 2017-2023, Microchip Technology Inc., and its subsidiaries. All rights reserved.
 
-Microchip Technology Inc. and its subsidiaries.
+The software and documentation is provided by microchip and its contributors
+"as is" and any express, implied or statutory warranties, including, but not
+limited to, the implied warranties of merchantability, fitness for a particular
+purpose and non-infringement of third party intellectual property rights are
+disclaimed to the fullest extent permitted by law. In no event shall microchip
+or its contributors be liable for any direct, indirect, incidental, special,
+exemplary, or consequential damages (including, but not limited to, procurement
+of substitute goods or services; loss of use, data, or profits; or business
+interruption) however caused and on any theory of liability, whether in contract,
+strict liability, or tort (including negligence or otherwise) arising in any way
+out of the use of the software and documentation, even if advised of the
+possibility of such damage.
 
-Subject to your compliance with these terms, you may use Microchip software 
-and any derivatives exclusively with Microchip products. It is your 
-responsibility to comply with third party license terms applicable to your 
-use of third party software (including open source software) that may 
-accompany Microchip software.
-
-THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
-WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A PARTICULAR 
-PURPOSE.
-
-IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
-BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE 
-FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN 
-ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY, 
-THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
-*****************************************************************************/
+Except as expressly permitted hereunder and subject to the applicable license terms
+for any third-party software incorporated in the software and any applicable open
+source software license terms, no license or other rights, whether express or
+implied, are granted under any patent or other intellectual property rights of
+Microchip or any third party.
+*/
 
 //DOM-IGNORE-END
 
@@ -58,9 +56,13 @@ THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 #include "tcpip/tcpip_ethernet.h"
 #include "driver/gmac/src/dynamic/_gmac_dcpt_lists.h"
 #if defined (DRV_ETH)
-	#include "driver/gmac/src/dynamic/drv_gmac_lib_pic32cz.h"
+    #if defined (DRV_PIC32CZ)
+        #include "driver/gmac/src/dynamic/drv_gmac_lib_pic32cz.h"
+    #elif defined (DRV_PIC32CK)
+        #include "driver/gmac/src/dynamic/drv_gmac_lib_pic32ck.h"
+    #endif
 #else
-	#include "driver/gmac/src/dynamic/drv_gmac_lib_sam.h"
+    #include "driver/gmac/src/dynamic/drv_gmac_lib_sam.h"
 #endif
 #include "driver/gmac/src/drv_gmac_local.h"
 #include "device.h"
@@ -987,6 +989,43 @@ void DRV_PIC32CGMAC_LibTxAckPendPacket( DRV_GMAC_DRIVER * pMACDrv, GMAC_QUE_LIST
     <p>Replaces:<p><c><b>void DRV_PIC32CGMAC_LibTxAckErrPacket( DRV_GMAC_DRIVER * pMACDrv, GMAC_QUE_LIST queueIdx, TCPIP_MAC_PKT_ACK_RES ackRes)</b></c>
  *****************************************************************************/
 void DRV_PIC32CGMAC_LibTxAckErrPacket( DRV_GMAC_DRIVER * pMACDrv, GMAC_QUE_LIST queueIdx, TCPIP_MAC_PKT_ACK_RES ackRes);
+
+/*******************************************************************************
+  Function:
+    void DRV_PIC32CGMAC_LibTxClearUnAckPacket( DRV_GMAC_DRIVER * pMACDrv, GMAC_QUE_LIST queueIdx, TCPIP_MAC_PKT_ACK_RES ackRes);
+ 
+  Summary:
+    Acknowledge the Unacknowledged Transmit packets during transmit errors
+
+  Description:
+    This function acknowledges packets added to MAC transmit descriptors. This function will
+    acknowledge all the unacknowledged transmitted or not transmitted packets in transmit descriptor. 
+    This will be called during error condition to discard queued TX packets. 
+    The TX acknowledgment function will be called from this routine.
+
+  Precondition:
+    None
+
+  Parameters:
+    pMACDrv         - driver instance.
+    queueIdx        - queue Index
+    ackRes          - packet acknowledgment result
+
+  Returns:
+    None
+
+  Example:
+    <code>
+    DRV_PIC32CGMAC_LibTxClearUnAckPacket(pMACDrv, queueIdx, ackRes); 
+    </code>
+
+  Remarks:
+    Any TX packets in transmit descriptors has to be acknowledged during error condition, 
+    otherwise the memory allocated for the packet will not be freed.
+
+    <p>Replaces:<p><c><b>void DRV_PIC32CGMAC_LibTxClearUnAckPacket( DRV_GMAC_DRIVER * pMACDrv, GMAC_QUE_LIST queueIdx, TCPIP_MAC_PKT_ACK_RES ackRes)</b></c>
+ *****************************************************************************/
+void DRV_PIC32CGMAC_LibTxClearUnAckPacket( DRV_GMAC_DRIVER * pMACDrv, GMAC_QUE_LIST queueIdx, TCPIP_MAC_PKT_ACK_RES ackRes);
 // *****************************************************************************
 // *****************************************************************************
 // Section: Packet receive functions
