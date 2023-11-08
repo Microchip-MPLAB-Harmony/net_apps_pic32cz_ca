9af826148b626672f9059361bd392d1d91869e08
Mohammed Fahad R <mohammedfahad.r@microchip.com>
2023-11-08 09:54:01 +0530

    [MH3-86288] tcpip_tcp_client application regeneration - PIC32CZ_CA_freertos

--- apps/tcpip_tcp_client/firmware/src/config/pic32cz_ca80_curiosity_ultra_freertos/configuration.h
+++ apps/tcpip_tcp_client/firmware/src/config/pic32cz_ca80_curiosity_ultra_freertos/configuration.h
@@ -93,7 +93,7 @@ extern "C" {
 
 #define SYS_CMD_ENABLE
 #define SYS_CMD_DEVICE_MAX_INSTANCES       SYS_CONSOLE_DEVICE_MAX_INSTANCES
-#define SYS_CMD_PRINT_BUFFER_SIZE          2560
+#define SYS_CMD_PRINT_BUFFER_SIZE          2560U
 #define SYS_CMD_BUFFER_DMA_READY
 
 /* Command System Service RTOS Configurations*/
@@ -107,10 +107,10 @@ extern "C" {
 #define SYS_DEBUG_USE_CONSOLE
 
 
-#define SYS_CONSOLE_DEVICE_MAX_INSTANCES   			1
-#define SYS_CONSOLE_UART_MAX_INSTANCES 	   			2
-#define SYS_CONSOLE_USB_CDC_MAX_INSTANCES 	   		0
-#define SYS_CONSOLE_PRINT_BUFFER_SIZE        		200
+#define SYS_CONSOLE_DEVICE_MAX_INSTANCES   			(1U)
+#define SYS_CONSOLE_UART_MAX_INSTANCES 	   			(1U)
+#define SYS_CONSOLE_USB_CDC_MAX_INSTANCES 	   		(0U)
+#define SYS_CONSOLE_PRINT_BUFFER_SIZE        		(200U)
 
 
 
@@ -301,6 +301,7 @@ extern "C" {
 
 #define TCPIP_STACK_TICK_RATE		        		5
 #define TCPIP_STACK_SECURE_PORT_ENTRIES             10
+#define TCPIP_STACK_LINK_RATE		        		333
 
 #define TCPIP_STACK_ALIAS_INTERFACE_SUPPORT   false
 
@@ -328,6 +329,7 @@ extern "C" {
 
 /*** ETH Configuration ***/
 #define DRV_ETH
+#define DRV_PIC32CZ
 #define TCPIP_ETH_TX_DESCRIPTORS_COUNT_DUMMY    1
 #define TCPIP_ETH_RX_DESCRIPTORS_COUNT_DUMMY    1
 #define TCPIP_ETH_RX_BUFF_SIZE_DUMMY            64
